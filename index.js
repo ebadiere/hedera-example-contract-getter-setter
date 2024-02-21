748326614a73935de6df1b90a7512201d501379a
ebadiere <ebadiere@gmail.com>
2024-02-21 12:03:23 -0700

    Enhanced example to reproduce an issue with deleting contracts.
    Signed-off-by: ebadiere <ebadiere@gmail.com>

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ const {
 	AccountId,
 	PrivateKey,
 	Client,
-	FileCreateTransaction,
-	ContractCreateTransaction,
+	ContractDeleteTransaction,
 	ContractFunctionParameters,
 	ContractExecuteTransaction,
 	ContractCallQuery,
-	Hbar,
 	ContractCreateFlow,
 } = require("@hashgraph/sdk");
 const fs = require("fs");
@@ -25,7 +23,8 @@ async function main() {
 	const contractBytecode = fs.readFileSync("LookupContract_sol_LookupContract.bin");
 
 	// Instantiate the smart contract
-	const contractInstantiateTx = new ContractCreateFlow()
+	let contractInstantiateTx = new ContractCreateFlow()
+	.setAdminKey(operatorKey)
 		.setBytecode(contractBytecode)
 		.setGas(200000)
 		.setConstructorParameters(new ContractFunctionParameters().addString("Alice").addUint256(111111));
@@ -46,21 +45,64 @@ async function main() {
 	console.log(`- Here's the phone number that you asked for: ${contractQueryResult} \n`);
 
 	// Call contract function to update the state variable
-	const contractExecuteTx = new ContractExecuteTransaction()
+	let contractExecuteTx = new ContractExecuteTransaction()
 		.setContractId(contractId)
 		.setGas(100000)
 		.setFunction("setMobileNumber", new ContractFunctionParameters().addString("Bob").addUint256(222222));
-	const contractExecuteSubmit = await contractExecuteTx.execute(client);
-	const contractExecuteRx = await contractExecuteSubmit.getReceipt(client);
+	let contractExecuteSubmit = await contractExecuteTx.execute(client);
+	let contractExecuteRx = await contractExecuteSubmit.getReceipt(client);
 	console.log(`- Contract function call status: ${contractExecuteRx.status} \n`);
 
 	// Query the contract to check changes in state variable
-	const contractQueryTx1 = new ContractCallQuery()
+	let contractQueryTx1 = new ContractCallQuery()
 		.setContractId(contractId)
 		.setGas(100000)
 		.setFunction("getMobileNumber", new ContractFunctionParameters().addString("Bob"));
 	const contractQuerySubmit1 = await contractQueryTx1.execute(client);
 	const contractQueryResult1 = contractQuerySubmit1.getUint256(0);
 	console.log(`- Here's the phone number that you asked for: ${contractQueryResult1} \n`);
+
+	console.log(`- Deleting the smart contract... \n`);
+
+	const contractDeleteTx = new ContractDeleteTransaction()
+    .setContractId(contractId)
+    .setTransferAccountId(operatorId)
+    .freezeWith(client);
+
+    // Sign the transaction with the client operator private key
+    const signedTx = await contractDeleteTx.sign(operatorKey);
+
+    // Submit the transaction to the Hedera network
+    const txResponse = await signedTx.execute(client);
+
+    // Request the receipt of the transaction
+    const receipt = await txResponse.getReceipt(client);
+
+    // Log the transaction status
+    console.log("The transaction status is:", receipt.status.toString()); 
+
+	console.log(`- The smart contract has been deleted. \n`);
+	console.log(`- The smart contract ID is: ${contractId} \n`);
+	console.log(`- The smart contract ID in Solidity format is: ${contractAddress} \n`);
+	console.log(`- About to update phone number \n`);
+
+	// Call contract function to update the state variable
+	contractExecuteTx = new ContractExecuteTransaction()
+		.setContractId(contractId)
+		.setGas(100000)
+		.setFunction("setMobileNumber", new ContractFunctionParameters().addString("Jim").addUint256(333333));
+	contractExecuteSubmit = await contractExecuteTx.execute(client);
+	contractExecuteRx = await contractExecuteSubmit.getReceipt(client);
+	console.log(`- Contract function call status: ${contractExecuteRx.status} \n`);
+
+	// Query the contract to check changes in state variable
+	contractQueryTx1 = new ContractCallQuery()
+		.setContractId(contractId)
+		.setGas(100000)
+		.setFunction("getMobileNumber", new ContractFunctionParameters().addString("Bob"));
+	contractQuerySubmit1 = await contractQueryTx1.execute(client);
+	contractQueryResult1 = contractQuerySubmit1.getUint256(0);
+	console.log(`- Here's the phone number that you asked for: ${contractQueryResult1} \n`);
 }
+
 main();
